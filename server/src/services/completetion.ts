7036aa7df423fc9d246de39958be4ec3f7ebf93d
Nicholas Perell <nicholasperell@gmail.com>
2025-07-16 13:26:09 -0400

    Converted to class with member variables and broken up functions

--- server/src/services/completetion.ts
+++ server/src/services/completetion.ts
@@ -1,98 +1,143 @@
 import { Position, CompletionList, CompletionItem, CompletionItemKind, TextEdit, InsertTextFormat, Color } from "vscode-languageserver";
-import { Element, NodeType, Program } from "../parsing/uxmlNodes";
+import { Element, Node, NodeType, Program } from "../parsing/uxmlNodes";
 import { Scanner } from "../parsing/uxmlScanner";
 import { Range, TextDocument } from "vscode-languageserver-textdocument";
 import { off } from "process";
+import { Token } from "../parsing/uxmlTokens";
 
 export function doCompletion(document: TextDocument, position: Position, info: (s: string) => void): CompletionList {
-    const scanner = new Scanner(document);
-    const program = new Program(scanner);
-    const offset = document.offsetAt(position);
-    const currentWord = scanner.getCurrentWord(offset);
-    const currentToken = scanner.getCurrentToken(offset);
-    const range = { start: document.positionAt(offset - currentWord.length), end: position };
-    const encases = program.addIfEncasing(offset);
-    const currentNode = encases[0];
+    const cmp = new Completion(document, position, info);
+    return cmp.getResult();
+}
 
-    const result: CompletionList = {
-        isIncomplete: false,
-        itemDefaults: {
-            editRange: {
-                start: { line: position.line, character: position.character - currentWord.length },
-                end: position
+class Completion {
+    private document: TextDocument;
+    private position: Position;
+    private info: (s: string) => void;
+    private scanner: Scanner;
+    private program: Program;
+    private offset: number;
+    private currentWord: string;
+    private nsEngine?: string;
+    private nsEditor?: string;
+    private currentToken?: Token;
+    private range: Range;
+    private encases: Node[];
+    private currentNode: Node;
+    private result: CompletionList;
+    private fullText: string;
+
+    public constructor(document: TextDocument, position: Position, info: (s: string) => void) {
+        this.document = document;
+        this.position = position;
+        this.info = info;
+
+        this.scanner = new Scanner(document);
+        this.program = new Program(this.scanner);
+        this.offset = document.offsetAt(position);
+        this.fullText = document.getText();
+        this.currentWord = this.scanner.getCurrentWord(this.offset);
+        this.currentToken = this.scanner.getCurrentToken(this.offset);
+        this.range = { start: document.positionAt(this.offset - this.currentWord.length), end: position };
+        this.encases = this.program.addIfEncasing(this.offset);
+        this.currentNode = this.encases[0];
+        const currentNode = this.currentNode;
+
+        this.result = {
+            isIncomplete: false,
+            itemDefaults: {
+                editRange: {
+                    start: { line: position.line, character: position.character - this.currentWord.length },
+                    end: position
+                }
+            },
+            items: []
+        };
+
+        info(`${this.offset} ${NodeType[currentNode?.type]} "${this.currentWord}" ${this.program.root !== undefined}`);
+
+
+        this.nsEngine = this.program.nsEngine;
+        this.nsEditor = this.program.nsEditor;
+
+        if (currentNode?.type === NodeType.Name) {
+            this.inNameCompletion();
+        } else if (currentNode?.type === NodeType.Namespace) {
+            this.inNamespaceCompletion();
+        } else if (currentNode instanceof Element) {
+            this.inElementCompletion();
+        } else if (currentNode?.type === NodeType.LeafElement || currentNode?.type === NodeType.StartElement) {
+            this.inLeafStartElementCompletion();
+        } else if (currentNode?.type === undefined) {
+            this.inUndefinedCompletion();
+        }
+    }
+
+    private inNameCompletion() {
+
+    }
+
+    private inNamespaceCompletion() {
+
+    }
+
+    private inElementCompletion() {
+        const offset = this.offset;
+        const currentWord = this.currentWord;
+
+        if (
+            offset <= currentWord.length ||
+            offset < 1
+        ) {
+            return;
+        }
+
+        const fullText = this.fullText;
+        const currentNode = this.currentNode as Element;
+        const nsEngine = this.nsEngine;
+        const nsEditor = this.nsEditor;
+        const range = this.range;
+
+        const beforeIndex = offset - currentWord.length - 1;
+        const before = fullText[beforeIndex];
+
+        if (before === '<') {
+            this.pushCompletionsAtStartOpenAngle();
+        } else if (before === ':') {
+            let nsStartIndex = beforeIndex;
+
+            while (nsStartIndex > 0 && fullText[nsStartIndex - 1].match(/[a-zA-Z]/)) {
+                nsStartIndex--;
             }
-        },
-        items: []
-    };
 
-    info(`${offset} ${NodeType[currentNode?.type]} "${currentWord}" ${program.root !== undefined}`);
-
-
-    const nsEngine = program.nsEngine;
-    const nsEditor = program.nsEditor;
-
-    if (currentNode?.type === NodeType.Name) {
-
-    } else if (currentNode?.type === NodeType.Namespace) {
-
-    } else if (currentNode instanceof Element) {
-
-        const fullText = document.getText();
-
-        if (offset > currentWord.length && currentWord.length > 0) {
-            const beforeIndex = offset - currentWord.length - 1;
-            const before = fullText[beforeIndex];
-            if (before === '<') {
-                if (!!nsEngine && nsEngine !== '') {
-                    result.items.push(doNameSpaceCompletion(range, nsEngine));
-                } else {
-                    result.items.push(...doElementCompletion(range, engineElements));
-                }
-
-                if (!!nsEditor && nsEditor !== '') {
-                    result.items.push(doNameSpaceCompletion(range, nsEditor));
-                } else {
-                    result.items.push(...doElementCompletion(range, editorElements));
-                }
-            } else if (before === ':') {
-                let woah = beforeIndex;
-                for (let i = beforeIndex - 1; i >= 0; i--) {
-                    if (!fullText[i].match(/[a-zA-Z]/)) {
-                        break;
-                    }
-
-                    woah = i;
-                }
-
-                const ns = woah === beforeIndex
-                    ? ''
-                    : fullText.substring(woah, beforeIndex);
-
-                if (ns === '') {
-
-                } else if (!!nsEngine && ns === nsEngine) {
-                    result.items.push(...doElementCompletion(range, engineElements));
-                } else if (!!nsEngine && ns === nsEditor) {
-                    result.items.push(...doElementCompletion(range, editorElements));
-                }
+            if (
+                nsStartIndex === beforeIndex ||
+                fullText[nsStartIndex - 1] !== '<'
+            ) {
+                return;
             }
-        } else if (offset > 1 && fullText[offset - 1] === '>' && fullText[offset - 2] !== '/') {
 
+            const ns = fullText.substring(nsStartIndex, beforeIndex);
+
+            if (!!nsEngine && ns === nsEngine) {
+                this.result.items.push(...doElementCompletion(range, engineElements));
+            } else if (!!nsEngine && ns === nsEditor) {
+                this.result.items.push(...doElementCompletion(range, editorElements));
+            }
+        } else if (offset > 1 && before === '>' && fullText[beforeIndex - 2] !== '/') {
             const startElementEnd = currentNode.startElement.getEnd();
-
             const lastIndexOpen = fullText.lastIndexOf('<', offset);
 
             if (
-                lastIndexOpen !== startElementEnd &&
+                lastIndexOpen > startElementEnd &&
                 fullText[lastIndexOpen + 1].match(/[a-zA-Z]/) &&
-                program.addIfEncasing(lastIndexOpen)[0] === currentNode
+                this.program.addIfEncasing(lastIndexOpen)[0] === currentNode
             ) {
                 const after = fullText.substring(lastIndexOpen + 1);
-
                 const elementName = after.match(/[a-zA-Z]+(:[a-zA-Z]+)?/)![0];
                 const name = `</${elementName} >`
-                info(name);
-                result.items.push({
+                this.info(name);
+                this.result.items.push({
                     label: name,
                     kind: CompletionItemKind.Reference,
                     insertTextFormat: InsertTextFormat.PlainText,
@@ -100,57 +145,26 @@ export function doCompletion(document: TextDocument, position: Position, info: (
                 });
             }
 
+
         }
+    }
 
-    } else if (currentNode?.type === NodeType.LeafElement || currentNode?.type === NodeType.StartElement) {
-        // const rulesStored = currentNode.getChildNodes();
-        // let start = currentNode.getStart();
-        // let end = currentNode.getEnd();
+    private inLeafStartElementCompletion() {
 
-        // rulesStored.forEach(element => {
-        //     const ruleStart = element.getStart();
-        //     const ruleEnd = element.getEnd();
+    }
 
-        //     if (offset < ruleStart && ruleStart < end) {
-        //         end = ruleStart;
-        //     } else if (offset > ruleEnd && ruleEnd > start) {
-        //         start = ruleEnd;
-        //     }
-        // });
+    private inUndefinedCompletion() {
+        const fullText = this.fullText;
+        const offset = this.offset;
+        const currentWord = this.currentWord;
+        const nsEngine = this.nsEngine;
+        const nsEditor = this.nsEditor;
 
-        // const fullText = document.getText();
-        // let indexColon = fullText.indexOf(':', start);
-
-        // if (indexColon < offset && indexColon > start) {
-        //     const identifier = fullText.substring(indexColon, start).trim();
-
-        //     let keywords = getKeywordsForProperty(identifier);
-
-        //     if (keywords.values.length == 0 && keywords.methods.length == 0) {
-        //         keywords = { values: [...valueKeywords, ...colorKeywords], methods: [] };
-        //     }
-
-        //     result.items = turnKeywordsToCompletions(range, keywords);
-        // } else {
-        //     result.items = doPropertyNameCompletion(range, indexColon < end);
-        // }
-    } else if (currentNode?.type === undefined) {
-        const fullText = document.getText();
         if (offset > currentWord.length) {
             const beforeIndex = offset - currentWord.length - 1;
             const before = fullText[beforeIndex];
             if (before === '<') {
-                if (!!nsEngine && nsEngine !== '') {
-                    result.items.push(doNameSpaceCompletion(range, nsEngine));
-                } else {
-                    result.items.push(...doElementCompletion(range, engineElements));
-                }
-
-                if (!!nsEditor && nsEditor !== '') {
-                    result.items.push(doNameSpaceCompletion(range, nsEditor));
-                } else {
-                    result.items.push(...doElementCompletion(range, editorElements));
-                }
+                this.pushCompletionsAtStartOpenAngle();
             } else if (before === ':') {
                 let woah = beforeIndex;
                 for (let i = beforeIndex - 1; i >= 0; i--) {
@@ -168,16 +182,31 @@ export function doCompletion(document: TextDocument, position: Position, info: (
                 if (ns === '') {
 
                 } else if (!!nsEngine && ns === nsEngine) {
-                    result.items.push(...doElementCompletion(range, engineElements));
-                } else if (!!nsEngine && ns === nsEditor) {
-                    result.items.push(...doElementCompletion(range, editorElements));
+                    this.result.items.push(...doElementCompletion(this.range, engineElements));
+                } else if (!!nsEditor && ns === nsEditor) {
+                    this.result.items.push(...doElementCompletion(this.range, editorElements));
                 }
             }
         }
     }
 
+    private pushCompletionsAtStartOpenAngle() {
+        if (!!this.nsEngine && this.nsEngine !== '') {
+            this.result.items.push(doNameSpaceCompletion(this.range, this.nsEngine));
+        } else {
+            this.result.items.push(...doElementCompletion(this.range, engineElements));
+        }
 
-    return result;
+        if (!!this.nsEditor && this.nsEditor !== '') {
+            this.result.items.push(doNameSpaceCompletion(this.range, this.nsEditor));
+        } else {
+            this.result.items.push(...doElementCompletion(this.range, editorElements));
+        }
+    }
+
+    public getResult() {
+        return this.result;
+    }
 }
 
 export function doCompletionResolve(item: CompletionItem) {
