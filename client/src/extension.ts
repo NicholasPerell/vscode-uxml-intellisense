330be14b97c79435d3f30b4731e3f993eaa421af
Nicholas Perell <nicholasperell@gmail.com>
2025-07-17 15:12:23 -0400

    Fix up lang name

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -29,7 +29,7 @@ export function activate(context: ExtensionContext) {
   // Options to control the language client
   const clientOptions: LanguageClientOptions = {
     // Register the server for all documents by default
-    documentSelector: [{ scheme: "file", language: "uxml" }],
+    documentSelector: [{ scheme: "file", language: "UnityUXML" }],
     synchronize: {
       // Notify the server about file changes to '.clientrc files contained in the workspace
       fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
